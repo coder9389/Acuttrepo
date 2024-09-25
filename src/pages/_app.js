a147229551ea3d62c2d0aa5acb05f9cecd20729a
coder9389 <bhaweshsammal12@gmail.com>
2024-09-26 01:43:36 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import { MantineProvider } from '@mantine/core';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+};
+
+export default MyApp;
